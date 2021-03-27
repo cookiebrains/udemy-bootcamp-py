228465b4cf786acf212f66231ea00a0221bf3e30
Amy Thompson <amy.lindsay@gmail.com>
2021-03-27 17:53:57 -0600

    14

--- input_files/__init__.py
+++ input_files/__init__.py
@@ -1,2 +1,4 @@
 from .hangman_words import word_list
 from .calc_art import logo
+from .higher_lower_game_data import data
+from .higher_lower_art import hl_logo, vs
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utilz import MyStopwatch
-from projects import project_12 as problem
+from projects import project_14 as problem
 
 
 def get_problem_num():
